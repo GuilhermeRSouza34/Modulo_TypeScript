66e798eb020e89c553465fb89c21e92e4afacf22
Guilherme <guilhermersouza34@gmail.com>
2024-04-18 19:37:25 -0300

    Aula Sobre os Tipos do TypeScript

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,21 @@
+let estaChovendo: boolean = false;
+estaChovendo = true;
+
+let idade: number = 20;
+let altura: number = 1.75;
+
+const nacionalidade: string = "Brasileira";
+const colegas: string[] = ['Gabriel', 'Kaike', 'Ana'];
+const tecnologias: Array<string> = ['html', 'json', 'css', 'js'];
+const notas: ReadonlyArray<number> = [1, 2, 3, 4, 5];
+
+const lista: [Nome: string,estaEstudando: boolean,idade: number] = ['Guilherme', true, 20]
+
+let idadeDaAna: number | string = 30;
+idadeDaAna = '30 Anos';
+
+let dadosDaAPI: any;
+dadosDaAPI = 10;
+dadosDaAPI = true;
+dadosDaAPI = [1,2,3];
+dadosDaAPI = 'String';
